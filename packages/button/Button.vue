eac1e2d42d3266fb37aa439d3249431f5440771d
刘银亮 <lyliang@liuyinliangdeMacBook-Pro.local>
2023-07-17 23:02:52 +0800

    fix: 修复禁用状态切换后不生效问题

--- packages/button/Button.vue
+++ packages/button/Button.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 interface ButtonGradientProps {
   type?: "normal" | "gradient" | "gradientText";
   rounded?: boolean;
@@ -6,19 +8,24 @@ interface ButtonGradientProps {
 }
 
 const props = withDefaults(defineProps<ButtonGradientProps>(), {
-  disable: false,
   type: "normal",
+  rounded: false,
+  disable: false,
 });
 // 圆角
-const roundedClass = props.rounded ? " v-rounded" : "";
+const roundedClass = computed(() => (props.rounded ? " v-rounded" : ""));
 // 禁用
-const disableClass = props.disable ? " v-disable" : "";
+const disableClass = computed(() => (props.disable ? " v-disable" : ""));
 // 渐变
-const gradientClass =
-  props.type === "gradient" && !props.disable ? " v-gradient" : "";
+const gradientClass = computed(() =>
+  props.type === "gradient" && !props.disable ? " v-gradient" : ""
+);
 // 文字渐变
-const gradientTextClass =
-  props.type === "gradientText" && !props.disable ? " v-gradient-text" : "";
+const gradientTextClass = computed(() =>
+  props.type === "gradientText" && !props.disable
+    ? " v-gradient v-gradient-text"
+    : ""
+);
 </script>
 
 <template>
@@ -50,7 +57,6 @@ const gradientTextClass =
   border-radius: 6px;
   .v-button-span {
     padding: 0 16px;
-    font-size: 16px;
     font-style: normal;
     font-weight: 500;
     color: #262626;
@@ -69,10 +75,12 @@ const gradientTextClass =
     background-clip: content-box, border-box;
   }
   &.v-gradient-text {
-    background: linear-gradient(to right, #981f0b, #cc2a0e, #981f0b);
-    background-clip: text;
-    -webkit-background-clip: text;
-    color: transparent;
+    .v-button-span {
+      background: linear-gradient(to right, #981f0b, #cc2a0e, #981f0b);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    }
   }
 }
 .v-button + .v-button {
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { Button } from "../packages/index";
+
+const disable = ref<boolean>(false);
 </script>
 
 <template>
-  <Button class="button">普通</Button>
-  <Button class="button" rounded>普通圆角</Button>
-  <Button class="button" type="gradient">渐变</Button>
-  <Button class="button" type="gradientText" rounded>文字渐变</Button>
-  <Button class="button" disable>禁用</Button>
+  <div>
+    <Button class="button" @click="disable = !disable">普通</Button>
+    <Button class="button" rounded>普通圆角</Button>
+    <Button class="button" type="gradient">渐变</Button>
+    <Button class="button" type="gradientText" rounded :disable="disable">
+      文字渐变
+    </Button>
+    <Button class="button" :disable="disable">禁用</Button>
+  </div>
 </template>
 
 <style scoped lang="scss">
